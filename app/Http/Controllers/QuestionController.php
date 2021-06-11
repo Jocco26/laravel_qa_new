c99a0b35351e4808a8a2d1b9eb78be4669d90302
unknown <joks026@gmail.com>
2021-06-11 21:42:03 +0800

    aw

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -55,7 +55,9 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        $question->increment('views');
+
+        return view('questions.show', compact('question'));
     }
 
     /**
